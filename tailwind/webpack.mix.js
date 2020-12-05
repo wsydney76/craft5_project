555d5207a50e788c96abdf6113e2b370cd028ec7
wsydn <wsydney@web.de>
2020-12-05 13:43:59 +0100

    Replace tailwind setup in order to work with the first party tailwind plugin in PhpStorm 2020.3

--- tailwind/webpack.mix.js
+++
@@ -1,8 +0,0 @@
-const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss');
-
-mix.sass('./src/styles/style.scss', '../web/assets/dist/main.css')
-    .options({
-        processCssUrls: false,
-        postCss: [ tailwindcss('./tailwind.config.js') ],
-    })
