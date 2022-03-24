5c16188ba42c8e71e0030007429cfebe89066293
wsydn <wsydney@web.de>
2022-03-24 17:57:58 +0100

    Fixes

--- install.bat
+++ install.bat
@@ -4,5 +4,5 @@ set /P handle=Enter handle:
 composer install --no-dev &&^
 php setup-temp.php %handle% &&^
 php craft install --interactive=0 --username=admin --password=password  --email=admin@example.com &&^
-php craft migrate/all --interactive=0 --no-backup
-
+php craft migrate/all --interactive=0 --no-backup &&^
+npm install
