6eca7cea44d95d5436525fae214996150c337606
wsydn <wsydney@web.de>
2022-04-20 10:47:28 +0200

    Image macro

---
+++ config/custom.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'defaultTransform' => [
+        'width' => 1024,
+        'height' => 500,
+        'format' => 'webp'
+    ],
+    'defaultSrcSet' => [
+        '1024w',
+        '640w',
+        '360w'
+    ]
+];
