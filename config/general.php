b1f20a22a67ddda4b1535785b1fe73d9e8b53dba
wsydn <wsydney@web.de>
2024-02-10 11:34:17 +0100

    Post section with CKEditor

--- config/general.php
+++ config/general.php
@@ -15,39 +15,35 @@ $isDev = App::env('CRAFT_ENVIRONMENT') === 'dev';
 $isProd = App::env('CRAFT_ENVIRONMENT') === 'production';
 
 return
-	GeneralConfig::create()
-		->devMode($isDev)
-		->allowAdminChanges($isDev)
-
+    GeneralConfig::create()
+        ->devMode($isDev)
+        ->allowAdminChanges($isDev)
         ->preloadSingles()
-
-		->maxRevisions(10)
-		->defaultWeekStartDay(1)
-
-		->omitScriptNameInUrls()
-		->cpTrigger('admin')
-		->limitAutoSlugsToAscii()
-
-		->preventUserEnumeration()
-		->sendPoweredByHeader(false)
-		->disallowRobots(!$isProd)
-
-		->defaultTemplateExtensions(['twig'])
-		->enableTemplateCaching($isProd)
-
-		->convertFilenamesToAscii()
-		->maxUploadFileSize('32M')
-		->generateTransformsBeforePageLoad()
-		->optimizeImageFilesize(false)
-		->revAssetUrls()
+        ->maxRevisions(10)
+        ->defaultWeekStartDay(1)
+        ->omitScriptNameInUrls()
+        ->cpTrigger('admin')
+        ->limitAutoSlugsToAscii()
+        ->preventUserEnumeration()
+        ->sendPoweredByHeader(false)
+        ->disallowRobots(!$isProd)
+        ->defaultTemplateExtensions(['twig'])
+        ->enableTemplateCaching($isProd)
+        ->convertFilenamesToAscii()
+        ->maxUploadFileSize('32M')
+        ->generateTransformsBeforePageLoad()
+        ->optimizeImageFilesize(false)
+        ->revAssetUrls()
         ->tempAssetUploadFs('tempimages_local')
 
-		// ->useIframeResizer(true)
+        // ->useIframeResizer(true)
 
-		->aliases([
-			// Prevent the @web alias from being set automatically (cache poisoning vulnerability)
-			'@web' => App::env('PRIMARY_SITE_URL'),
-			// Lets `./craft clear-caches all` clear CP resources cache
-			'@webroot' => dirname(__DIR__) . '/web',
-		]);
+        ->disabledUtilities(['find-replace', 'db-backup'])
+
+        ->aliases([
+            // Prevent the @web alias from being set automatically (cache poisoning vulnerability)
+            '@web' => App::env('PRIMARY_SITE_URL'),
+            // Lets `./craft clear-caches all` clear CP resources cache
+            '@webroot' => dirname(__DIR__) . '/web',
+        ]);
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors'
 
 export default {
   content: [
       "./templates/**/*.{twig,svg}"
   ],
   theme: {
-    extend: {},
+    extend: {
+        screens: {
+            ml: '892px'
+        },
+
+        colors: {
+            gray: colors.slate
+        }
+    },
   },
   plugins: [
       require('@tailwindcss/typography')
